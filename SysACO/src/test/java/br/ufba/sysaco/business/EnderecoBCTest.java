96400d7324987ca955ee99fea3a20ce660e1871d
andredhou <andredhou@d69d7977-7436-b744-e1da-707a3c5915f8>
2011-02-12 02:19:11 +0000

    Mais testes para a entidade Endereço.

--- SysACO/src/test/java/br/ufba/sysaco/business/EnderecoBCTest.java
+++ SysACO/src/test/java/br/ufba/sysaco/business/EnderecoBCTest.java
@@ -7,6 +7,8 @@ import static org.junit.Assert.assertNotNull;
 
 import javax.inject.Inject;
 
+import org.drools.command.assertion.AssertEquals;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -41,12 +43,53 @@ public class EnderecoBCTest {
 	/**
 	 * Test method for {@link br.gov.frameworkdemoiselle.template.DelegateCrud#insert(java.lang.Object)}.
 	 */
+	
+	/*
+	 * Testes da camada de DAO.
+	 */
 	@Test
 	public void testInsert() {
 		Endereco endereco = getMinimalValidEndereco();
 		bc.insert(endereco);
 		assertNotNull(endereco.getId());
 	}
+	
+	@Test	
+	public void testUpdate() {
+		Endereco endereco = getMinimalValidEndereco();
+		bc.insert(endereco);
+		String novoNumero = "15";
+		endereco.setNumero("15");
+		bc.update(endereco);
+		endereco = bc.load(endereco.getId());
+		Assert.assertEquals(novoNumero, endereco.getNumero());
+	}
+	
+	@Test	
+	public void testDelete() {
+		Endereco endereco = getMinimalValidEndereco();
+		bc.insert(endereco);
+		endereco = bc.load(endereco.getId());	
+		bc.delete(endereco.getId());		
+		Assert.assertNull(endereco);
+	}
+	
+	@Test
+	public void testInsertLogradouroNull() {
+		Endereco endereco = getMinimalValidEndereco();
+		endereco.setLogradouro(null);
+		bc.insert(endereco);
+		Assert.assertNull(endereco.getId());
+	}
+	
+	/*
+	 * Testes da camada de negócio.
+	 */
+	
+	/**
+	 * Este método retorna um objeto que é minimamente válido.
+	 * @return Objeto mínimo possível
+	 */
 
 	private Endereco getMinimalValidEndereco() {
 		Endereco endereco = new Endereco();
